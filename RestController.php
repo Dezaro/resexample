12b292589fa5f2f77e42df041afc7e45d05b8312
Dezaro <Dezaro@192.168.2.64>
2016-07-04 10:32:50 +0300

    api code optimization

--- RestController.php
+++ RestController.php
@@ -1,29 +1,28 @@
 <?php
+
 require_once("MobileRestHandler.php");
-		
+
+$mobileRestHandler = new MobileRestHandler();
 $view = "";
-if(isset($_GET["view"])){
-	$view = $_GET["view"];
+
+if(isset($_GET["view"])) {
+  $view = $_GET["view"];
 }
+
 /*
-controls the RESTful services
-URL mapping
-*/
-switch($view){
-
-	case "all":
-		// to handle REST Url /mobile/list/
-		$mobileRestHandler = new MobileRestHandler();
-		$mobileRestHandler->getAllMobiles();
-		break;
-		
-	case "single":
-		// to handle REST Url /mobile/show/<id>/
-		$mobileRestHandler = new MobileRestHandler();
-		$mobileRestHandler->getMobile($_GET["id"]);
-		break;
-
-	case "" :
-		//404 - not found;
-		break;
+  controls the RESTful services
+  URL mapping
+ */
+switch($view) {
+  case "all":
+    // to handle REST Url /mobile/list/
+    $mobileRestHandler->getAllMobiles();
+    break;
+  case "single":
+    // to handle REST Url /mobile/show/<id>/
+    $mobileRestHandler->getMobile($_GET["id"]);
+    break;
+  case "" :
+    //404 - not found;
+    break;
 }
